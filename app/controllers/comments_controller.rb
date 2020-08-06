2d447ec4d35fe993ca8e88b018e2e44d36b61dee
Orane31 <orane.chaper@gmail.com>
2020-08-07 00:59:12 +0200

    dropdown navbar done and city choice added in the new user form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
     end
 
     def show
-        @comment = Comment.find(params[:id])
+
     end
 
     def new
@@ -13,12 +13,12 @@ class CommentsController < ApplicationController
     end
 
     def create
-        @comment = Comment.new(content: params[:content], user: User.find(53), gossip: Gossip.find(params[:gossip_id]))
+        @comment = Comment.new(content: params[:content], user: current_user.id, gossip: Gossip.find(params[:gossip_id]))
 
         if @comment.save
             redirect_to gossip_path(id: Gossip.find(params[:gossip_id])
         else 
-            render new_gossip_comment_path
+            render gossip_path(id: Gossip.find(params[:gossip_id])
         end
 
 
