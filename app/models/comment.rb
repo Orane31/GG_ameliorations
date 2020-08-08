3e82aded51d71fbffb58f1684cb02f78d69e96ac
Orane31 <orane.chaper@gmail.com>
2020-08-08 11:46:43 +0200

    comments are back

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,6 @@ class Comment < ApplicationRecord
     belongs_to :gossip
     belongs_to :user
     validates :content, 
-        presence: true, 
-        length: {minimum: 5, maximum: 200 }
+        presence: true,
 
 end
